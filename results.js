63bc8904f2e7b57253a628fae4f998c54a116b43
zhuqi6 <mary.zhu@mail.utoronto.ca>
2016-10-09 09:09:54 -0400

    new update

--- results.js
+++ results.js
@@ -1,127 +1,47 @@
 import React, { Component, PropTypes } from 'react';
 import { Linking, View, Text, TouchableHighlight, AppRegistry, StyleSheet, Image } from 'react-native';
 import { Container, Header, Title, Content, Button, Icon } from 'native-base';
+
 var Dimensions = require('Dimensions');
 var { width, height } = Dimensions.get('window');
-var obj = "Toronto Transit Comission"
-var originalText = "The Toronto Transit Commission (TTC) is a public transport agency that operates transit bus, streetcar, paratransit, and rapid transit services in Toronto, Ontario, Canada. Established in 1921, the TTC comprises four rapid transit lines with 69 stations, over 149 bus routes, and 10 streetcar lines.";
-var sentenceEnd = originalText.indexOf('.');
-// var text;
-// var count = 0;
-// while (sentenceEnd && count < 3) {
-//     text = originalText.splice(0, sentenceEnd + 1);
-//     count ++;
-// }
 
-
-export default class Results extends Component {
+export default class MyScene extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     onForward: PropTypes.func.isRequired,
     onBack: PropTypes.func.isRequired,
   }
-  _handlePress() {
-    Linking.openURL("https://google.ca").catch(err => console.error('An error occurred', err));
-  }
   render() {
     return (
-      <Container>
-      <Header>
-        {/*<Button transparent>
-          <Icon name='ios-menu' />
-        </Button>*/}
-      </Header>
-            <Content>
-              <View style={{flex: 1 }} >
-                <View style={{flex: 5}} >
-                  <Image style={styles.image} source={require('./img/unionrail.jpg')} />
-                    <View style={[styles.overlay, { height: 100}]} >
-                      <Text style={styles.overlayText} >
-                        {obj}
-                      </Text>
-                    </View>
-                  </View>
-        <View style={styles.infoBox} >
-            <Image style={ styles.image } source={require('./img/speechBubble.png')} style={{width: 350, height: 200, top:-80}} />
-            <Text style={styles.infoBoxText} >
-                {originalText}
-            </Text>
-        </View>
-        <View style={{flex: 1}} >
-            <Button primary style={styles.learnMore}
-            onPress={() => this._handlePress()}>
-              Learn More
-            </Button>
-            <Button success style={styles.next}
-            onPress={() => this._handlePress()}>
-              Success
-            </Button>
-        </View>
-      </View>
-    </Content>
-  </Container>
-    )
-  }
+      <Container> 
+        <Content>
+            <Image style={styles.image} source={require('./img/unionrail.jpg')} />
+            <Image style={ styles.confirmButton } source={require('./img/confirm.png')} />
+            <Image style={ styles.declineButton } source={require('./img/decline.png')} />
+        </Content>
+      </Container>
+  )}
 }
 
 var styles = StyleSheet.create({
   image: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-    borderWidth: 1,
-    flexDirection:'row',
-    width: width
-  },
-  overlay: {
-    flex: 1,
-    position: 'absolute',
-    left: 0,
-    bottom: 0,
-    opacity: 0.5,
-    backgroundColor: 'white',
+    resizeMode: 'cover',
     width: width,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: height
   },
-  overlayText: {
-    marginRight: 10,
-    marginLeft: 10,
-    fontSize: 18,
-    color: 'blue',
-    // fontFamily: 'Raleway',
-    fontWeight: 'bold',
-  },
-  infoBox: {
-    flex: 3,
-    top: 100,
-    backgroundColor: 'white',
-  },
-  infoBoxText: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    backgroundColor: 'transparent',
-    padding: 10,
-    paddingTop: 0,
+  confirmButton: {
     position: 'absolute',
-    top: -60,
-    left: 10,
-    right: 100,
-    width: 350,
-    // fontFamily: 'Raleway',
-    fontSize: 16
-
+    height: 75,
+    width: 75,
+    right: 90,
+    bottom: 75,
   },
-  learnMore: {
-    position: 'relative',
-    top: 100,
-    left: 50
-  },
-  next: {
+  declineButton: {
     position: 'absolute',
-    top: 100,
-    left: 200
+    height: 75,
+    width: 75,
+    left: 90,
+    bottom: 75,
   }
-});
+})
